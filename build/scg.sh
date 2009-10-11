f1730d5625b7e77670fed912339ae69f9d4e8bc9
gaberudy <gaberudy@7ea4356c-fe3d-0410-aa3e-7f05b20d65c6>
2009-10-11 16:21:08 +0000

    Updated build scripts to handle Mac and source builds

--- build/scg.sh
+++ build/scg.sh
@@ -15,24 +15,31 @@ svn update $source_dir
 
 [configure]
 PRINT Configuring source
-cd $source_dir; qmake -t vcapp $pro_file
+cd $source_dir; qmake ${qmake_args} $pro_file
 
 [make-windows]
 PRINT Makeing
 cd $source_dir; devenv.com ${product_name_abbrev}.vcproj /build "Release|Win32"
+
+[make-mac]
+PRINT Makeing
+cd $source_dir; make -j2
+cd $source_dir; macdeployqt ${product_binary_name}
 
 [clean_stagearea]
 PRINT Cleaning up old Stage Area (all data will be lost)...
 rm -rf $stagearea_dir
 mkdir -p $stagearea_dir
 
-
 [prepare_stage]
 PRINT Preparing stage area
 cp $source_dir/GPL.txt $stagearea_dir/
 cp $source_dir/README.txt $stagearea_dir/
+cp -R ${source_dir}${package_subdir}/${product_binary_name} $stagearea_dir/
+
+
+[prepare_stage_win]
 cp $source_dir/pthreadVSE2.dll $stagearea_dir/
-cp $source_dir/release/${product_binary_name} $stagearea_dir/
 cp $QTDIR/bin/QtGui4.dll $stagearea_dir/
 cp $QTDIR/bin/QtCore4.dll $stagearea_dir/
 mkdir -p $stagearea_dir/plugins/imageformats/
@@ -43,7 +50,7 @@ echo "[Paths]" > $stagearea_dir/qt.conf
 echo "plugins=plugins" >> $stagearea_dir/qt.conf
 
 
-[setup_package]
+[setup_package_win]
 PRINT Making the installer in ${local_package_dir}
 echo "#define MyAppName \"${product_name}\"" > ${source_dir}/install_script.iss
 echo "#define MyAppNameAbbrev \"${product_name_abbrev}\"" >> ${source_dir}/install_script.iss
@@ -63,3 +70,26 @@ tail -n +13 ${source_dir}/build/setup.iss >> ${source_dir}/install_script.iss
 "$istool_exe" -compile ${source_dir}/install_script.iss
 chmod a+rx ${local_package_dir}/${final_package_name}
 
+
+[setup_package_mac]
+PRINT Making the installer in ${local_package_dir}
+mkdir -p $local_package_dir
+rm -f ${local_package_dir}/${final_package_name}
+PRINT Zipping up our directory
+rm -rf "/tmp/${product_full_name}"
+cp -R ${stagearea_dir} "/tmp/${product_full_name}"
+cd /tmp; tar -czf ${final_package_name} "${product_full_name}"
+mv /tmp/${final_package_name} ${local_package_dir}/
+rm -rf "/tmp/${product_full_name}"
+
+[package_src]
+PRINT Making the installer in ${local_package_dir}
+mkdir -p $local_package_dir
+rm -f ${local_package_dir}/${final_package_name}
+PRINT Zipping up our directory
+rm -rf "/tmp/${product_full_name}"
+cp -R ${source_dir} "/tmp/${product_full_name}"
+/usr/bin/find "/tmp/${product_full_name}" -name ".svn" -exec rm -rf {} +
+cd /tmp; tar -czf ${final_package_name} "${product_full_name}"
+mv /tmp/${final_package_name} ${local_package_dir}/
+rm -rf "/tmp/${product_full_name}"
